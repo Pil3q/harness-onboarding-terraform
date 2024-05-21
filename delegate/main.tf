19baabb77cd894e395dd8c9a865913816626b771
bic-harness <bogdan.catana@harness.io>
2024-05-21 14:27:57 +0100

    Update main.tf

--- delegate/main.tf
+++ delegate/main.tf
@@ -1,7 +1,14 @@
 module "k8s_connector" {
   source  = "app.terraform.io/empire/modules/harness//modules/connectors/connector-kubernetes"
-  version = "1.0.0"
+  version = "1.0.1"
   team_name = var.team_name
   organization_id = var.organization_id
-  identifier = var.identifier
+  identifier = "var.identifier_${random_string.this.result}"
+}
+
+resource "random_string" "this" {
+  length = 4
+  special = false
+  upper = false
+  number = false
 }
